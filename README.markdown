48236ccac6626d1daae2f3fef2bfb43341677742
Warren Moore <warren@wamonite.com>
2015-04-27 20:23:10 +0100

    Initial working script.

---
+++ README.markdown
@@ -0,0 +1,19 @@
+# iTerm2 Colour Export
+
+Parses a binary [iTerm2](http://iterm2.com/) plist preferences file and exports all profile and preset colour schemes
+to a directory.
+
+[source @ github](https://github.com/wamonite/iterm2_colour_export)
+
+## License
+
+Copyright (c) 2015 Warren Moore
+
+This software may be redistributed under the terms of the MIT License.
+See the file LICENSE for details.
+
+## Contact
+
+          @wamonite     - twitter
+           \_______.com - web
+    warren____________/ - email
---
+++ iterm2_colour_export.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""iTerm2 Colour Export
+
+Parses a binary iTerm2 plist preferences file and exports all profile and preset colour schemes to a directory.
+
+Warren Moore
+      @wamonite     - twitter
+       \_______.com - web
+warren____________/ - email
+"""
+
+from __future__ import print_function
+import argparse
+import sys
+import biplist
+import os
+
+DEFAULT_PLIST = '~/Library/Preferences/com.googlecode.iterm2.plist'
+COLOUR_NAME_LIST = [
+    'Background Color',
+    'Foreground Color',
+    'Cursor Color',
+    'Cursor Text Color',
+    'Ansi 0 Color',
+    'Ansi 1 Color',
+    'Ansi 2 Color',
+    'Ansi 3 Color',
+    'Ansi 4 Color',
+    'Ansi 5 Color',
+    'Ansi 6 Color',
+    'Ansi 7 Color',
+    'Ansi 8 Color',
+    'Ansi 9 Color',
+    'Ansi 10 Color',
+    'Ansi 11 Color',
+    'Ansi 12 Color',
+    'Ansi 13 Color',
+    'Ansi 14 Color',
+    'Ansi 15 Color'
+]
+
+
+class ScriptException(Exception):
+    """Derived exception to throw simple error messages"""
+
+
+def get_rgb_colour(colour_value_lookup):
+    """Convert to integer RGB values to ignore rounding errors.
+    """
+
+    return (
+        int(float(colour_value_lookup['Red Component']) * 255.0),
+        int(float(colour_value_lookup['Green Component']) * 255.0),
+        int(float(colour_value_lookup['Blue Component']) * 255.0)
+    )
+
+
+def match_profile_to_preset(profile_lookup, colour_preset_lookup):
+    """Get a list of preset names that match the current profile colour scheme.
+    """
+
+    match_list = []
+    for preset_name, preset_colours in colour_preset_lookup.iteritems():
+        match = True
+        for colour_name, colour_val in preset_colours.iteritems():
+            if colour_name in profile_lookup:
+                if get_rgb_colour(colour_val) != get_rgb_colour(profile_lookup[colour_name]):
+                    match = False
+                    break
+
+        if match:
+            match_list.append(preset_name)
+
+    return match_list
+
+
+def print_match_profiles(profile_list, colour_preset_lookup):
+    """For each profile, print the list of colour presets it matches.
+    """
+
+    for profile_lookup in profile_list:
+        match_list = match_profile_to_preset(profile_lookup, colour_preset_lookup)
+        if match_list:
+            print('profile (%s) matches (%s)' % (profile_lookup['Name'], ', '.join(match_list)))
+
+
+def make_subdirectory(output_directory, subdirectory_name):
+    if not os.path.isdir(output_directory):
+        raise ScriptException('directory not found (%s)' % output_directory)
+
+    export_directory_name = os.path.join(output_directory, subdirectory_name)
+    if not os.path.isdir(export_directory_name):
+        os.mkdir(export_directory_name)
+
+    return export_directory_name
+
+
+def write_colour_plist(colour_lookup, export_name, colour_type_name, output_directory):
+    """Write the colour scheme to a named file as a text plist.
+    """
+
+    export_directory_name = make_subdirectory(output_directory, colour_type_name)
+    export_file_name = os.path.join(export_directory_name, '%s.plist' % export_name)
+    print('writing (%s) (%s) to (%s)' % (colour_type_name, export_name, export_file_name))
+
+    output_colour_lookup = {}
+    for colour_name in COLOUR_NAME_LIST:
+        if colour_name in colour_lookup:
+            output_colour_lookup[colour_name] = colour_lookup[colour_name]
+
+    biplist.writePlist(output_colour_lookup, export_file_name, binary = False)
+
+
+def export_colours(profile_list, colour_preset_lookup, output_directory):
+    """Write the profile and preset colour schemes to the output directory.
+    """
+
+    if profile_list:
+        for profile_lookup in profile_list:
+            write_colour_plist(profile_lookup, profile_lookup['Name'], 'profile', output_directory)
+
+    if colour_preset_lookup:
+        for preset_name, colour_lookup in colour_preset_lookup.iteritems():
+            write_colour_plist(colour_lookup, preset_name, 'preset', output_directory)
+
+
+def parse_iterm_plist(plist_file_name, match_profiles = True, output_directory = None):
+    """Parse the profile and preset colour schemes from the specified iTerm2 plist.
+    """
+
+    file_name = os.path.expanduser(plist_file_name)
+    plist = biplist.readPlist(file_name)
+
+    profile_list = plist.get('New Bookmarks', [])
+    colour_preset_lookup = plist.get('Custom Color Presets', {})
+
+    if match_profiles:
+        print_match_profiles(profile_list, colour_preset_lookup)
+
+    if output_directory:
+        export_colours(profile_list, colour_preset_lookup, output_directory)
+
+
+def do_iterm2_colour_export():
+    parser = argparse.ArgumentParser(
+        description = 'iterm2_colour_export',
+        formatter_class = argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument('-p', '--plist', default = DEFAULT_PLIST, help = 'input plist file')
+    parser.add_argument('-n', '--no-match', action = 'store_false', help = 'do not match profiles against presets')
+    parser.add_argument('-o', '--output-directory', help = 'output directory')
+    args = parser.parse_args()
+
+    parse_iterm_plist(
+        args.plist,
+        args.no_match,
+        args.output_directory
+    )
+
+
+if __name__ == "__main__":
+    try:
+        do_iterm2_colour_export()
+
+    except ScriptException as e:
+        print('Error:', e, file = sys.stderr)
+        sys.exit(1)
